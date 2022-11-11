5b4c3e11e0c73a64964226cbe23744e3ff4f8d12
Gerard <gerardpc2004@gmail.com>
2022-11-11 10:07:19 +0100

    Ejercicios de estructuras

---
+++ ejercicios_estructuras.py
@@ -0,0 +1,25 @@
+#Ejercicio 1
+lista = ['huevos','pan','leche','queso','pizza','papel wc']
+print(lista)
+print(len(lista)) #len = longitud de la lista 
+print(lista[0]) #print(lista[x]) = mostrar elemento de la lista
+lista[0] = 'huevos XL' #lista[x] = y --> cambiar elemento lista
+print(lista)
+lista.append('abuelo') #Añadir elemento
+print(lista)
+lista.pop(1) #Eliminar elemento
+print(lista)
+
+#Ejercicio 2 
+tupla = ('The Legend of Zelda: Breath of the Wild', 'Elden Ring', 'Dark Souls', 'God of War', 'Spiderman')
+
+#Ejercicio 3 
+datos_gery = {
+    'Nombre': 'Gerard',
+    'Edad': '18',
+    'Deporte favorito': 'Baloncesto',
+    'Enemigo mortal': 'Hitler'
+    }
+
+#Ejercicio 4
+habilidades_yasuo = set(['Camino del alma errante', 'Tempestad de acero', 'Muro de viento', 'Hoja cortante', 'Último aliento'])
